f294420a9bcb39450e8a11824e89fca8ae616456
Robert Scott Dionne <robertsdionne@gmail.com>
2014-07-28 20:51:39 -0400

    Fix constexpr methods in BloomFilter to be defined inline.

--- source/bloomfilter.h
+++ source/bloomfilter.h
@@ -1,15 +1,21 @@
 #ifndef VOXELS_BLOOMFILTER_H_
 #define VOXELS_BLOOMFILTER_H_
 
+#include <cmath>
+
 namespace voxels {
 
   template <int n, int pd>
   class BloomFilter {
   public:
 
-    constexpr int m() const;
+    constexpr int m() const {
+      return -n * log(1.0 / pd) / log(2.0) / log(2.0);
+    }
 
-    constexpr int k() const;
+    constexpr int k() const {
+      return static_cast<double>(m()) / n * log(2.0);
+    }
   };
 
 }  // namespace voxels
