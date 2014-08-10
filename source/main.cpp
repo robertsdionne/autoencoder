0857096c527261e7ab28b892d9f554319598a3b0
Robert Scott Dionne <robertsdionne@gmail.com>
2014-08-10 19:56:55 -0400

    Remove unnecessary libraries.

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,6 @@
-#include <GLXW/glxw.h>
-#include <GLFW/glfw3.h>
+#include <OpenCL/opencl.h>
 #include <iostream>
 
-#include "autoencoderrenderer.h"
-#include "glfwapplication.h"
-#include "mouse.h"
-
 int main(int argument_count, char *arguments[]) {
-  rsd::Mouse mouse;
-  autoencoder::AutoencoderRenderer renderer(mouse);
-  rsd::GlfwApplication application(
-      argument_count, arguments, 1024, 1024, 7, "Bloom Filters", renderer, mouse);
-  application.Run();
   return 0;
 }
