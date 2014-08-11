c154a6ac871275dda5759ac72eff1f42834382d2
Robert Scott Dionne <robertsdionne@gmail.com>
2014-08-10 21:05:31 -0400

    Include Eigen3.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,13 @@
+#include <Eigen/Dense>
 #include <cassert>
 #include <iostream>
 
 int main(int argument_count, char *arguments[]) {
+  Eigen::MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
   return 0;
 }
