9cdae6f4cb3250ae032a22439d610c26cd7c1169
Robert Scott Dionne <robertsdionne@gmail.com>
2014-09-01 14:52:15 -0400

    Link to gflags-static. Write usage.

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,10 @@
+#include <gflags/gflags.h>
 #include <iostream>
 
 #include "rectifiedlinearlayer.h"
 
 int main(int argument_count, char *arguments[]) {
+  gflags::SetUsageMessage("Part-of-speech tagger implemented with a recurrent neural network.");
+  gflags::ParseCommandLineFlags(&argument_count, &arguments, true);
   return 0;
 }
