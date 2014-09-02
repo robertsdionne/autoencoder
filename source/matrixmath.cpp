a4e6f80e7a1aa4650bd6f755593201abeb96ed4d
Robert Scott Dionne <robertsdionne@gmail.com>
2014-09-02 16:24:42 -0400

    Use cblas_sgemm.

--- source/matrixmath.cpp
+++ source/matrixmath.cpp
@@ -6,16 +6,10 @@
 namespace autoencoder {
 
   void Sgemm(float alpha, const Values &A, const Values &B, float beta, Values *C) {
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 1, 1, 1, 1.0f, A.values, 1,
-        B.values, 1, 0.0f, C->values, 1);
-    for (auto i = 0; i < A.height; ++i) {
-      for (auto j = 0; j < B.width; ++j) {
-        C->value(j, i) *= beta;
-        for (auto k = 0; k < A.width; ++k) {
-          C->value(j, i) += alpha * B.value(j, k) * A.value(k, i);
-        }
-      }
-    }
+    cblas_sgemm(
+        CblasColMajor, CblasNoTrans, CblasNoTrans,
+        A.height, B.width, A.width,
+        1.0f, A.values, A.height, B.values, B.height, 0.0f, C->values, C->height);
   }
 
 }  // namespace autoencoder
