75cd3ebaafbff68f00c9f6b93172ea7ffd115541
Robert Scott Dionne <robertsdionne@gmail.com>
2014-09-02 15:03:48 -0400

    Wire up OpenBLAS.

--- source/matrixmath.cpp
+++ source/matrixmath.cpp
@@ -1,9 +1,13 @@
+#include <cblas.h>
+
 #include "matrixmath.h"
 #include "values.h"
 
 namespace autoencoder {
 
   void Sgemm(float alpha, const Values &A, const Values &B, float beta, Values *C) {
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 1, 1, 1, 1.0f, A.values, 1,
+        B.values, 1, 0.0f, C->values, 1);
     for (auto i = 0; i < A.height; ++i) {
       for (auto j = 0; j < B.width; ++j) {
         C->value(j, i) *= beta;
