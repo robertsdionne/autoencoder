563bd73cef4865eed1ee8464a8c0192bcaba0716
Robert Scott Dionne <robertsdionne@gmail.com>
2014-09-27 18:32:47 -0400

    Fix VexCL compilation by updating cl.hpp. Write a unit test to experiment with VexCL's API. Pull in VexCL into the CMake configuration files.

---
+++ tests/vexcl_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <vexcl/vexcl.hpp>
+#include <vector>
+
+TEST(VexClTest, TestVexCl) {
+  vex::Context context{vex::Filter::Type{CL_DEVICE_TYPE_GPU}};
+  std::vector<float> h(10, 5.0f);
+  vex::vector<float> d(context, 10);
+  vex::copy(h, d);
+  {
+    auto mapped_pointer = d.map(0);
+    for (auto i = 0; i < d.part_size(0); ++i) {
+      EXPECT_FLOAT_EQ(5.0f, mapped_pointer[i]);
+    }
+  }
+}
